793ae80ef667e72bae8776d30631629a68569cf3
João Ribeiro Bezerra <joaorb64@gmail.com>
2021-03-07 18:08:03 -0300

    Fix font

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -15,7 +15,7 @@ export default class Body extends React.Component {
                     <meta charSet="utf-8"/>
                     <meta name="viewport" content="width=device-width, initialScale=1.0" />
                     <meta name="description" content={_.get(this.props, 'pageContext.frontmatter.excerpt', null) || _.get(this.props, 'pageContext.site.siteMetadata.description', null)}/>
-                    <link href="https://fonts.googleapis.com/css2?family=Alegreya&display=swap" rel="stylesheet" />
+                    <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
                     <link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,700,700i&display=swap" rel="stylesheet"/> 
                     {(_.get(this.props, 'pageContext.frontmatter.template', null) === 'post') && ( 
                     _.get(this.props, 'pageContext.frontmatter.canonical_url', null) && (
