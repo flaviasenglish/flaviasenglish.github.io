8c7ce32e2d0cc100d64ea31f781d4f94955d209c
João Ribeiro Bezerra <joaorb64@gmail.com>
2021-04-11 13:31:06 -0300

    Fix social link

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,7 +10,10 @@ const withPrefix = require("./src/utils/withPrefix").default;
 exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
 
     setHeadComponents([
-        <link rel="shortcut icon" type="image/png"  href="./favicon.png" />
+        <link rel="shortcut icon" type="image/png"  href="./favicon.png" />,
+        <meta property="og:title" content="Flávia's English" />,
+        <meta property="og:image" content="https://www.flaviasenglish.com.br/images/logo_social.jpg" />,
+        <meta property="og:description" content="English related services: tradução, revisão e ensino / translation, proofreading, tutoring. Acesse e saiba mais." />
     ]);
 
     setPostBodyComponents([
