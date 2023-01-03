cd520bb6e44738c6ee872ad160c7d1080683abf5
flaviasenglish <joaorb64@gmail.com>
2023-01-03 18:17:55 +0000

    Fix seo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import { useTranslation } from 'next-export-i18n';
 import { NextSeo } from 'next-seo';
 
 import { Base } from '../templates/Base';
@@ -7,39 +6,35 @@ import { AppConfig } from '../utils/AppConfig';
 import 'material-icons/iconfont/material-icons.css';
 
 const Index = () => {
-  const { t } = useTranslation();
+  // const { t } = useTranslation();
 
   return (
     <>
       <NextSeo
         title={AppConfig.site_name}
-        description={`${t('event_description_title')} - ${t(
-          'event_description_pre'
-        )} R$ 60.000!`}
-        canonical="https://ultrahard.info"
+        description={``}
+        canonical="https://www.flaviaforcatho.com.br"
         openGraph={{
-          url: 'https://ultrahard.info',
+          url: 'https://www.flaviaforcatho.com.br/',
           title: AppConfig.site_name,
-          description: `${t('event_description_title')} - ${t(
-            'event_description_pre'
-          )} R$ 60.000!`,
+          description: ``,
           images: [
             {
-              url: `https://ultrahard.info/logo_1024.png`,
+              url: `https://www.flaviaforcatho.com.br/logo_1024.png`,
               width: 1024,
               height: 1024,
               alt: 'Flávia Forcatho English Solutions',
               type: 'image/png',
             },
             {
-              url: `https://ultrahard.info/logo_256.png`,
+              url: `https://www.flaviaforcatho.com.br//logo_256.png`,
               width: 256,
               height: 256,
               alt: 'Flávia Forcatho English Solutions',
               type: 'image/png',
             },
             {
-              url: `https://ultrahard.info/logo_tt.png`,
+              url: `https://www.flaviaforcatho.com.br/logo_tt.png`,
               width: 1200,
               height: 630,
               alt: 'Flávia Forcatho English Solutions',
@@ -49,8 +44,7 @@ const Index = () => {
           site_name: AppConfig.description,
         }}
         twitter={{
-          handle: '@tryhardsmash',
-          site: '@tryhardsmash',
+          site: 'https://www.flaviaforcatho.com.br/',
           cardType: 'summary_large_image',
         }}
       />
